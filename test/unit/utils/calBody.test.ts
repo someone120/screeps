56fd43ad873d9f26a6887a960e8186455989a6f6
someone120 <renmou533@gmail.com>
2021-03-20 00:07:59 +0800

    chore(package): 修改了测试用例

--- test/unit/utils/calBody.test.ts
+++ test/unit/utils/calBody.test.ts
@@ -1,9 +1,6 @@
 import { calcBodyPart } from '../../../src/utils';
-import { assert } from "chai";
 
 it('1move', () => {
     const body = calcBodyPart({ ['move']: 1 });
-    assert.isDefined(body)
-    assert.equal(body.length, 1)
-    assert.includeMembers(body, ['move'])
+    expect(body).toMatchObject(['move'])
 });
